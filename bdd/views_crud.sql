8a4da95b843f240041e55b4b6e8ad4bf661e8222
Lydrownn <151926776+Lydrownn@users.noreply.github.com>
2024-10-15 17:47:34 +0200

    Update views_crud.sql
    [A TESTER AVEC LE SCRIPT DE CREA] tout les create sont fait, plus qu'a potentielement update jsp on va voir emblessant

--- bdd/views_crud.sql
+++ bdd/views_crud.sql
@@ -94,16 +94,189 @@ VALUES('Co-7850','batttt@gmail.com','ouieuhhhcestmoi','26','rue du Prout','Saint
 -- Spectacle
 create or replace view saeTest.spectacle AS
   select * from saeTest._offre natural join saeTest._spectacle;
+
+create or replace function saeTest.createSpectacle()
+  RETURNS trigger
+  AS
+$$
+BEGIN
+  insert into saeTest._offre(idoffre,nomoffre,numadresse,
+                            rueoffre,villeoffre,codepostaloffre,
+                            prixmin,datedebut,datefin,enligne,datepublication,
+                            dernieremaj,estpremium,blacklistdispo,idcompte)
+  values(NEW.idoffre,NEW.nomoffre,NEW.numadresse,
+          NEW.rueoffre,NEW.villeoffre,NEW.codepostaloffre,
+          NEW.prixmin,NEW.datedebut,NEW.datefin,NEW.enligne,NEW.datepublication,
+          NEW.dernieremaj,NEW.estpremium,NEW.blacklistdispo,NEW.idcompte);
+  
+  insert into saeTest._spectacle(idoffre,dureespectacle,placesspectacle)
+  values(NEW.idoffre,NEW.dureespectacle,NEW.placesspectacle);
+
+  RETURN NEW;
+END;
+$$ language plpgsql;
+
+CREATE OR REPLACE TRIGGER tg_createSpectacle
+  INSTEAD OF INSERT ON saeTest.spectacle
+  FOR EACH ROW
+  EXECUTE PROCEDURE saeTest.createSpectacle();
+
+INSERT INTO saeTest.spectacle(idoffre,nomoffre,numadresse,rueoffre,villeoffre,codepostaloffre,
+                              prixmin,datedebut,datefin,enligne,datepublication,dernieremaj,estpremium,
+                              blacklistdispo,idcompte,dureespectacle,placesspectacle)
+VALUES('Of-0001','spectacle2oof','23','rue du Pipi','Pipiville',22300,60.0,
+        CURRENT_DATE,CURRENT_DATE,true,CURRENT_DATE,CURRENT_DATE,true,2,'Co-7850','123min',64);
 -- Parc d'attraction
 create or replace view saeTest.parcAttraction AS
   select * from saeTest._offre natural join saeTest._parcAttraction;
+
+create or replace function saeTest.createParcAttraction()
+  RETURNS trigger
+  AS
+$$
+BEGIN
+  insert into saeTest._offre(idoffre,nomoffre,numadresse,
+                            rueoffre,villeoffre,codepostaloffre,
+                            prixmin,datedebut,datefin,enligne,datepublication,
+                            dernieremaj,estpremium,blacklistdispo,idcompte)
+  values(NEW.idoffre,NEW.nomoffre,NEW.numadresse,
+          NEW.rueoffre,NEW.villeoffre,NEW.codepostaloffre,
+          NEW.prixmin,NEW.datedebut,NEW.datefin,NEW.enligne,NEW.datepublication,
+          NEW.dernieremaj,NEW.estpremium,NEW.blacklistdispo,NEW.idcompte);
+  
+  insert into saeTest._parcattraction(idoffre,urlversplan,nbattractions,ageminparc)
+  values(NEW.idoffre,NEW.urlversplan,NEW.nbattractions,NEW.ageminparc);
+
+  RETURN NEW;
+END;
+$$ language plpgsql;
+
+CREATE OR REPLACE TRIGGER tg_createParcAttraction
+  INSTEAD OF INSERT ON saeTest.parcAttraction
+  FOR EACH ROW
+  EXECUTE PROCEDURE saeTest.createParcAttraction();
+
+INSERT INTO saeTest.parcAttraction(idoffre,nomoffre,numadresse,rueoffre,villeoffre,codepostaloffre,
+                              prixmin,datedebut,datefin,enligne,datepublication,dernieremaj,estpremium,
+                              blacklistdispo,idcompte,urlversplan,nbattractions,ageminparc)
+VALUES('Of-0001','spectacle2oof','23','rue du Pipi','Pipiville',22300,60.0,
+        CURRENT_DATE,CURRENT_DATE,true,CURRENT_DATE,CURRENT_DATE,true,2,'Co-7850',
+        'https://lecacalepipilepopo/attractions',4,7);
+
+
 -- Visite
 create or replace view saeTest.visite AS
   select * from saeTest._offre natural join saeTest._visite;
+
+create or replace function saeTest.createVisite()
+  RETURNS trigger
+  AS
+$$
+BEGIN
+  insert into saeTest._offre(idoffre,nomoffre,numadresse,
+                            rueoffre,villeoffre,codepostaloffre,
+                            prixmin,datedebut,datefin,enligne,datepublication,
+                            dernieremaj,estpremium,blacklistdispo,idcompte)
+  values(NEW.idoffre,NEW.nomoffre,NEW.numadresse,
+          NEW.rueoffre,NEW.villeoffre,NEW.codepostaloffre,
+          NEW.prixmin,NEW.datedebut,NEW.datefin,NEW.enligne,NEW.datepublication,
+          NEW.dernieremaj,NEW.estpremium,NEW.blacklistdispo,NEW.idcompte);
+  
+  insert into saeTest._visite(idoffre,dureevisite,estguidee)
+  values(NEW.idoffre,NEW.dureevisite,NEW.estguidee);
+
+  RETURN NEW;
+END;
+$$ language plpgsql;
+
+CREATE OR REPLACE TRIGGER tg_createVisite
+  INSTEAD OF INSERT ON saeTest.visite
+  FOR EACH ROW
+  EXECUTE PROCEDURE saeTest.createVisite();
+
+INSERT INTO saeTest.visite(idoffre,nomoffre,numadresse,rueoffre,villeoffre,codepostaloffre,
+                              prixmin,datedebut,datefin,enligne,datepublication,dernieremaj,estpremium,
+                              blacklistdispo,idcompte,dureevisite,estguidee)
+VALUES('Of-0001','spectacle2oof','23','rue du Pipi','Pipiville',22300,60.0,
+        CURRENT_DATE,CURRENT_DATE,true,CURRENT_DATE,CURRENT_DATE,true,2,'Co-7850','5433min',true);
+
+
 -- Activite
 create or replace view saeTest.activite AS
   select * from saeTest._offre natural join saeTest._activite;
+
+create or replace function saeTest.createActivite()
+  RETURNS trigger
+  AS
+$$
+BEGIN
+  insert into saeTest._offre(idoffre,nomoffre,numadresse,
+                            rueoffre,villeoffre,codepostaloffre,
+                            prixmin,datedebut,datefin,enligne,datepublication,
+                            dernieremaj,estpremium,blacklistdispo,idcompte)
+  values(NEW.idoffre,NEW.nomoffre,NEW.numadresse,
+          NEW.rueoffre,NEW.villeoffre,NEW.codepostaloffre,
+          NEW.prixmin,NEW.datedebut,NEW.datefin,NEW.enligne,NEW.datepublication,
+          NEW.dernieremaj,NEW.estpremium,NEW.blacklistdispo,NEW.idcompte);
+  
+  insert into saeTest._activite(idoffre,urlversplan,agerequis,dureeactivite)
+  values(NEW.idoffre,NEW.urlversplan,NEW.agerequis,NEW.dureeactivite);
+
+  RETURN NEW;
+END;
+$$ language plpgsql;
+
+CREATE OR REPLACE TRIGGER tg_createActivite
+  INSTEAD OF INSERT ON saeTest.activite
+  FOR EACH ROW
+  EXECUTE PROCEDURE saeTest.createActivite();
+
+INSERT INTO saeTest.activite(idoffre,nomoffre,numadresse,rueoffre,villeoffre,codepostaloffre,
+                              prixmin,datedebut,datefin,enligne,datepublication,dernieremaj,estpremium,
+                              blacklistdispo,idcompte,agerequis,dureeactivite)
+VALUES('Of-0001','spectacle2oof','23','rue du Pipi','Pipiville',22300,60.0,
+        CURRENT_DATE,CURRENT_DATE,true,CURRENT_DATE,CURRENT_DATE,true,2,'Co-7850',
+        9,'1234min');
+
+
 -- Restauration
 create or replace view saeTest.restauration AS
   select * from saeTest._offre natural join saeTest._restauration;
   
+create or replace function saeTest.createRestauration()
+  RETURNS trigger
+  AS
+$$
+BEGIN
+  insert into saeTest._offre(idoffre,nomoffre,numadresse,
+                            rueoffre,villeoffre,codepostaloffre,
+                            prixmin,datedebut,datefin,enligne,datepublication,
+                            dernieremaj,estpremium,blacklistdispo,idcompte)
+  values(NEW.idoffre,NEW.nomoffre,NEW.numadresse,
+          NEW.rueoffre,NEW.villeoffre,NEW.codepostaloffre,
+          NEW.prixmin,NEW.datedebut,NEW.datefin,NEW.enligne,NEW.datepublication,
+          NEW.dernieremaj,NEW.estpremium,NEW.blacklistdispo,NEW.idcompte);
+  
+  insert into saeTest._restauration(idoffre,urlverscarte,gammeprix,moycuisine,moyservice,
+                                    moyambiance,moyrapportqp,petitdejeuner,dejeuner,diner,
+                                    boisson,brunch)
+  values(NEW.idoffre,NEW.urlverscarte,NEW.gammeprix,NEW.moycuisine,NEW.moyservice,
+         NEW.moyambiance,NEW.moyrapportqp,NEW.petitdejeuner,NEW.dejeuner,NEW.diner,
+         NEW.boisson,NEW.brunch);
+
+  RETURN NEW;
+END;
+$$ language plpgsql;
+
+CREATE OR REPLACE TRIGGER tg_createRestauration
+  INSTEAD OF INSERT ON saeTest.restauration
+  FOR EACH ROW
+  EXECUTE PROCEDURE saeTest.createRestauration();
+
+INSERT INTO saeTest.restauration(idoffre,nomoffre,numadresse,rueoffre,villeoffre,codepostaloffre,
+                              prixmin,datedebut,datefin,enligne,datepublication,dernieremaj,estpremium,
+                              blacklistdispo,idcompte,urlverscarte,gammeprix,moycuisine,moyservice,
+                              moyambiance,moyrapportqp,petitdejeuner,dejeuner,diner,boisson,brunch)
+VALUES('Of-0001','spectacle2oof','23','rue du Pipi','Pipiville',22300,60.0,
+        CURRENT_DATE,CURRENT_DATE,true,CURRENT_DATE,CURRENT_DATE,true,2,'Co-7850',
+        'https://lecacalepipilepopo/attractions','€€',4.4,4.4,4.4,4.4,true,true,true,true,true);
