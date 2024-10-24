1e909c8b1d5ef4e68d023fe3cbf43e8b2be618a4
Jan <92032537+Azhur65@users.noreply.github.com>
2024-10-24 14:33:00 +0200

    Ajout d'une offre Of-0001

--- bdd/creationTest.sql
+++ bdd/creationTest.sql
@@ -935,6 +935,14 @@ VALUES('Culture','Of-0007');
 
 -- Parc d'atraction
 
+INSERT INTO parcAttraction(idoffre,nomoffre,numadresse,rueoffre,villeoffre,codepostaloffre,
+prixmin,idcompte,resume,urlversplan)
+VALUES('Of-0001','Le Parc du Tregor,','4','Route du Tregor',
+'Lannion',22300,5.00,'Co-0003',
+'Parc d'attractions tout publique sur le thème de la bretagne, activités et lieu de restauration. (Ce parc est fictif)',
+'https://thumbs.dreamstime.com/b/carte-de-ville-imaginaire-avec-parcelle-cadastrale-parc-public-et-terrain-vert-naturel-illustration-cadastre-320575928.jpg');
+
+
 INSERT INTO compteProfessionnelPublique(idcompte,email,motdepasse,
 numadressecompte,ruecompte,villecompte,codepostalcompte,telephone,
 denomination)
