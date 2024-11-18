be0e1040b0b4915f99376e7e3e09e02e9f94fab3
Azhur64 <jan.donal.cox@gmail.com>
2024-11-18 14:36:11 +0100

    added valid connection check file

---
+++ html/verif_connection.inc.php
@@ -0,0 +1,22 @@
+<?php
+require_once "db_connection.inc.php";
+
+
+function valid_account()
+{
+    global $dbh;
+    try {
+        $query = "SELECT * FROM " . NOM_SCHEMA . "." . NOM_TABLE_COMPTE . ";";
+        $res = $dbh->query($query)->fetchAll();
+
+        $isValid = false;
+        $i = 0;
+        while ($i < sizeof($res) && !$isValid) {
+            if ($_SESSION['identifiant'] == $res[$i]['email']);
+        }
+    } catch (PDOException $e) {
+        echo "Couldn't fetch accounts : " . $e->getMessage();
+    }
+
+    return $isValid;
+}
