cb70c831ab13af9794a6da0c68d2e74dd99820c4
Azhur64 <jan.donal.cox@gmail.com>
2024-12-03 09:57:01 +0100

    partial update commit

--- html/afficher_avis.inc.php
+++ html/afficher_avis.inc.php
@@ -17,13 +17,44 @@ function afficher_liste_avis($id_offre)
 
 function afficher_avis($avis)
 {
+    $date_visite = getdate(strtotime($avis['datevisite']));
 ?>
     <div class="avis">
         <h3 class="titre_avis"><?php echo $avis['titre']; ?></h3>
         <p class="note_avis"> <?php echo $avis['noteavis'] . "/5"; ?> </p> <!-- a modifier avec le bon affichage de la note -->
-        <p class="date_visite"> <?php echo $avis
+        <p class="date_visite"> <?php echo $avis['datevisite']; ?> </p>
         <p class="contexte"> <?php echo $avis['contexte']; ?> </p>
         <p class="commentaire"><?php echo $avis['commentaire'] ?></p>
     </div>
 <?php
 }
+
+function get_jour($date_visite): string
+{
+    switch ($date_visite['wday']) {
+        case 0:
+            $ret = "Dimanche";
+            break;
+        case 1:
+            $ret = "Lundi";
+            break;
+        case 2:
+            $ret = "Mardi";
+            break;
+        case 3:
+            $ret = "Mercredi";
+            break;
+        case 4:
+            $ret = "Jeudi";
+            break;
+        case 5:
+            $ret = "Vendredi";
+            break;
+        case 6:
+            $ret = "Samedi";
+            break;
+    }
+    return $ret;
+}
+
+function get_mois(): string {}
